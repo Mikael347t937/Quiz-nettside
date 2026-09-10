1d3a66d7729ed066f102b1a211b67b69e45e936a
Mikael347t937 <mimia014@osloskolen.no>
2026-09-10 14:31:46 +0200

    la til update text

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const sporsmol =  ["Hva er en CPU","Hva er et motherboard","Hva er RAM","Hva er en GPU","hva er power suply","Hva er et cooling system",]
-const svar = ["a", "a", "c", "d"]
+
 
 const svar1 = document.getElementById("svar1")
 const svar2 = document.getElementById("svar2")
 const svar3 = document.getElementById("svar3")
 const svar4 = document.getElementById("svar4")
-
+const poeng_display = document.getElementById("poeng")
 const spor = document.getElementById("spor")
-svar1.addEventListener("click", () => {
-    spor.textContent = sporsmol[1]
-    
-})
+
+const svar_liste = [ 
+    ["Riktig", "feil", "feil,", "feil"], 
+    ["Riktig", "feil", "feil,", "feil"],
+    ["Riktig", "feil", "feil,", "feil"],
+    ["Riktig", "feil", "feil,", "feil"],
+ ]
+   
+
+let poeng = 0
+let spor_no = 0
+let svar = ["1", "3", "3", "2"]
+
+function update_questions(){
+    spor.textContent = sporsmol[spor_no]
+    svar1.textContent = svar_liste[spor_no][0]
+   svar2.textContent = svar_liste[spor_no][1]
+   svar3.textContent = svar_liste[spor_no][2]
+   svar4.textContent = svar_liste[spor_no][3]
+    poeng_display.textContent = points
+}
